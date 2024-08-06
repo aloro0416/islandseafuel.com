ef8fc8da038635f418ffa903a5977c4fb3882f28
aloro0416 <abellolordesaloro@gmail.com>
2024-08-06 14:48:29 +0800

    change

--- media.php
+++ media.php
@@ -36,7 +36,8 @@
               <div class="form-group">
                 <div class="input-group">
                   <span class="input-group-btn">
-                    <input type="file" name="file_upload" multiple="multiple" class="btn btn-primary btn-file">Add</span>
+                    <input type="file" name="file_upload" multiple="multiple" class="btn btn-primary btn-file"/>
+                 </span>
 
                  <button type="submit" name="submit" class="btn btn-default">Upload</button>
                </div>
