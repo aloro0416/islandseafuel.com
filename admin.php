57e60b2e90e71d66e1d602a28e5fceca69ba89fa
aloro0416 <abellolordesaloro@gmail.com>
2024-09-16 17:54:45 +0800

    change

--- admin.php
+++ admin.php
@@ -55,7 +55,7 @@
     <div class="col-md-4">
        <div class="panel panel-box clearfix">
          <div class="panel-icon pull-left bg-green">
-          <i class="glyphicon glyphicon-usd"></i>
+         <i class="fa fa-peso-sign"></i>
         </div>
         <div class="panel-value pull-right">
           <h2 class="margin-top"> <?php  echo $c_sale['total']; ?></h2>
