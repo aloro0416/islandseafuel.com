20573efbe8131afee6ee32a838d672e8d1e9e56d
aloro0416 <abellolordesaloro@gmail.com>
2024-09-28 17:11:05 +0800

    Update header.php

--- layouts/header.php
+++ layouts/header.php
@@ -9,6 +9,7 @@
            echo ucfirst($user['name']);
             else echo "Island Sea Management System";?>
     </title>
+    <link rel="shortcut icon" href="../uploads/logo.png" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
     <link rel="stylesheet" href="libs/css/main.css" />
