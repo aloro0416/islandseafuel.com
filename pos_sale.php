f76db7dc5661f6e593cbabf4e3c93ddcb31ead0e
aloro0416 <abellolordesaloro@gmail.com>
2024-09-28 16:14:57 +0800

    Update pos_sale.php

--- pos_sale.php
+++ pos_sale.php
@@ -1,10 +1,10 @@
 <?php
   $page_title = 'POS_SALE';
-  require_once('includes/load.php');
+  
   // Checkin What level user has permission to view this page
   page_require_level(2);
 ?>
-<?php include_once('layouts/header.php'); ?>
+
 
 
 <div class="row">
@@ -171,4 +171,3 @@
 
 </div>
 
-<?php include_once('layouts/footer.php'); ?>
