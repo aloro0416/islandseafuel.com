551e8fe83f9a535607f0e84e36bb98378a1a57b9
aloro0416 <abellolordesaloro@gmail.com>
2024-09-28 16:01:53 +0800

    Changed

--- pos.php
+++ pos.php
@@ -2,6 +2,7 @@
   $page_title = 'POS';
   require_once('includes/load.php');
   // Checkin What level user has permission to view this page
+  page_require_level(2);
 ?>
 <?php include_once('layouts/header.php'); ?>
 
