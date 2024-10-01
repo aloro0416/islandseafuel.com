2c9b716899506667b2f099f5e07c4de7b231b393
aloro0416 <abellolordesaloro@gmail.com>
2024-10-01 10:50:30 +0800

    Update header.php

--- layouts/header.php
+++ layouts/header.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
     <link rel="stylesheet" href="libs/css/main.css" />
+    <link rel="stylesheet" href="libs/css/datatables.min.css" />
     <link rel="stylesheet" href="dist/apexcharts.css" />
     <!-- SweetAlert CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
