9584192f5fd6f2b7e7e57ee1a80a3f010571218b
aloro0416 <abellolordesaloro@gmail.com>
2024-10-26 21:16:17 +0800

    Update header.php

--- layouts/header.php
+++ layouts/header.php
@@ -12,6 +12,7 @@
     <link rel="shortcut icon" href="uploads/logo.png" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="libs/css/main.css" />
     <link rel="stylesheet" href="libs/css/datatables.min.css" />
     <link rel="stylesheet" href="dist/apexcharts.css" />
