0befc1322c1783f316fdae09f46f48fba6c9a838
aloro0416 <abellolordesaloro@gmail.com>
2024-11-05 16:40:10 +0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -122,16 +122,7 @@
         }
         $product_sales_json_3 = json_encode($monthly_product_sales_3);
 		
-        $product_id_4 = 18; // Replace with actual product ID for the second line
-
-        $monthly_product_sales_4 = [];
-        foreach (['1' => 'Jan', '2' => 'Feb', '3' => 'Mar', '4' => 'Apr', '5' => 'May', '6' => 'Jun', '7' => 'Jul', '8' => 'Aug', '9' => 'Sep', '10' => 'Oct', '11' => 'Nov', '12' => 'Dec'] as $month_num => $month_name) {
-            $sql = "SELECT SUM(price) as total FROM sales WHERE MONTH(date) = $month_num AND product_id = '$product_id_4'";
-            $result = $db->query($sql);
-            $data = mysqli_fetch_assoc($result);
-            $monthly_product_sales_4[] = (int)$data['total'];
-        }
-        $product_sales_json_4 = json_encode($monthly_product_sales_4);
+       
 
 	
         }
