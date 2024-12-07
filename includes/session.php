e737da51dbafdd9ebb3e67c9feb27c63ca5ef130
aloro0416 <abellolordesaloro@gmail.com>
2024-12-08 01:07:02 +0800

    a

--- includes/session.php
+++ includes/session.php
@@ -1,59 +1,76 @@
 <?php
- session_start();
+session_start();
 
 class Session {
 
- public $msg;
- private $user_is_logged_in = false;
+    public $msg;
+    private $user_is_logged_in = false;
 
- function __construct(){
-   $this->flash_msg();
-   $this->userLoginSetup();
- }
-
-  public function isUserLoggedIn(){
-    return $this->user_is_logged_in;
-  }
-  public function login($user_id){
-    $_SESSION['user_id'] = $user_id;
-  }
-  private function userLoginSetup()
-  {
-    if(isset($_SESSION['user_id']))
-    {
-      $this->user_is_logged_in = true;
-    } else {
-      $this->user_is_logged_in = false;
+    function __construct() {
+        $this->userLoginSetup();
     }
 
-  }
-  public function logout(){
-    unset($_SESSION['user_id']);
-  }
-
-  public function msg($type ='', $msg =''){
-    if(!empty($msg)){
-       if(strlen(trim($type)) == 1){
-         $type = str_replace( array('d', 'i', 'w','s'), array('danger', 'info', 'warning','success'), $type );
-       }
-       $_SESSION['msg'][$type] = $msg;
-    } else {
-      return $this->msg;
+    public function isUserLoggedIn() {
+        return $this->user_is_logged_in;
     }
-  }
 
-  private function flash_msg(){
-
-    if(isset($_SESSION['msg'])) {
-      $this->msg = $_SESSION['msg'];
-      unset($_SESSION['msg']);
-    } else {
-      $this->msg;
+    public function login($user_id) {
+        $_SESSION['user_id'] = $user_id;
     }
-  }
+
+    private function userLoginSetup() {
+        if (isset($_SESSION['user_id'])) {
+            $this->user_is_logged_in = true;
+        } else {
+            $this->user_is_logged_in = false;
+        }
+    }
+
+    public function logout() {
+        unset($_SESSION['user_id']);
+    }
+
+    // Store message and type
+    public function msg($type = '', $msg = '') {
+        if (!empty($msg)) {
+            if (strlen(trim($type)) == 1) {
+                $type = str_replace(array('d', 'i', 'w', 's'), array('danger', 'info', 'warning', 'success'), $type);
+            }
+            $_SESSION['msg'][$type] = $msg;
+        } else {
+            return $this->msg;
+        }
+    }
+
+    // Use SweetAlert for the message display
+    private function flash_msg() {
+        if (isset($_SESSION['msg'])) {
+            $this->msg = $_SESSION['msg'];
+            unset($_SESSION['msg']);
+        } else {
+            $this->msg;
+        }
+    }
 }
 
 $session = new Session();
-$msg = $session->msg();
+
+// Example of using the msg method:
+$session->msg('s', 'Success! You have logged in.'); // 's' for success
 
 ?>
+
+<!-- Include SweetAlert CDN -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script>
+<?php if (isset($session->msg)): ?>
+    <?php foreach ($session->msg as $type => $message): ?>
+        Swal.fire({
+            icon: '<?php echo $type; ?>',
+            title: '<?php echo ucfirst($type); ?>',
+            text: '<?php echo $message; ?>'
+        });
+    <?php endforeach; ?>
+<?php endif; ?>
+</script>
