d982949d31a555d26fef3fc09a00ceeea64ca7ba
aloro0416 <abellolordesaloro@gmail.com>
2024-12-07 23:29:24 +0800

    a

--- account_recovery_otp.php
+++ account_recovery_otp.php
@@ -44,9 +44,9 @@ if (isset($_POST['send'])) {
                 $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
                 $mail->Username   = 'islandsea2001@gmail.com';              // SMTP username
-                $mail->Password   = 'bkggsnzgpxrzrovb';                     // SMTP password
-                $mail->SMTPSecure = 'ssl';                                  // Enable implicit TLS encryption
-                $mail->Port       = 465;                                    // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+                $mail->Password   = 'bkgg snzg pxrz rovb';                     // SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;                                  // Enable implicit TLS encryption
+                $mail->Port       = 587;                                    // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
                 
                 // Recipients
                 $mail->setFrom('islandsea2001@gmail.com', 'IslandSea');
