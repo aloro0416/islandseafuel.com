37e405cf820976937d00b7225bd117b83fd079a1
aloro0416 <abellolordesaloro@gmail.com>
2024-12-08 00:37:31 +0800

    a

--- edit_account.php
+++ edit_account.php
@@ -91,10 +91,6 @@
                   <label for="username" class="control-label">Username</label>
                   <input type="text" class="form-control" name="username" value="<?php echo remove_junk(ucwords($user['username'])); ?>">
             </div>
-            <div class="form-group">
-                  <label for="email" class="control-label">Email</label>
-                  <input type="text" class="form-control" name="email" value="<?php echo remove_junk(ucwords($user['email'])); ?>">
-            </div>
             <div class="form-group clearfix">
                     <a href="change_password" title="change password" class="btn btn-danger pull-right">Change Password</a>
                     <button type="submit" name="update" class="btn btn-info">Update</button>
