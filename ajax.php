5ad39dfbf4866d3701bb51a6ad0afc0da137981d
aloro0416 <abellolordesaloro@gmail.com>
2024-12-09 14:09:16 +0800

    a

--- ajax.php
+++ ajax.php
@@ -27,55 +27,39 @@
    }
  ?>
  <?php
- // find all products
-if (isset($_POST['p_name']) && strlen($_POST['p_name'])) {
-  $product_title = remove_junk($db->escape($_POST['p_name']));
-  if ($results = find_all_product_info_by_title($product_title)) {
-      foreach ($results as $result) {
+ // find all product
+  if(isset($_POST['p_name']) && strlen($_POST['p_name']))
+  {
+    $product_title = remove_junk($db->escape($_POST['p_name']));
+    if($results = find_all_product_info_by_title($product_title)){
+        foreach ($results as $result) {
 
-          // Get the price and quantity values from the form (or set defaults)
-          $price = isset($_POST['price']) ? $_POST['price'] : $result['sale_price'];
-          $quantity = isset($_POST['quantity']) ? $_POST['quantity'] : 1;
-
-          // Validate price (ensure it is a positive number)
-          if (!is_numeric($price) || $price <= 0) {
-              $html .= "<tr><td colspan='6' class='text-danger'>Invalid price entered for product: {$result['name']}. Please enter a valid number greater than zero.</td></tr>";
-              continue; // Skip this product and continue with the next
-          }
-
-          // Validate quantity (ensure it is a positive integer)
-          if (!is_numeric($quantity) || $quantity <= 0 || (int)$quantity != $quantity) {
-              $html .= "<tr><td colspan='6' class='text-danger'>Invalid quantity entered for product: {$result['name']}. Please enter a valid integer greater than zero.</td></tr>";
-              continue; // Skip this product and continue with the next
-          }
-
-          // If both price and quantity are valid, continue to generate the table row
           $html .= "<tr>";
 
-          $html .= "<td id=\"s_name\">" . $result['name'] . "</td>";
+          $html .= "<td id=\"s_name\">".$result['name']."</td>";
           $html .= "<input type=\"hidden\" name=\"s_id\" value=\"{$result['id']}\">";
-          $html .= "<td>";
-          $html .= "<input type=\"text\" class=\"form-control\" id=\"price\" name=\"price\" value=\"{$price}\">"; // Editable price
-          $html .= "</td>";
+          $html  .= "<td>";
+          $html  .= "<input type=\"text\" class=\"form-control\" name=\"price\" value=\"{$result['sale_price']}\">";
+          $html  .= "</td>";
           $html .= "<td id=\"s_qty\">";
-          $html .= "<input type=\"text\" class=\"form-control\" id=\"quantity\" name=\"quantity\" value=\"{$quantity}\">"; // Editable quantity
-          $html .= "</td>";
-          $html .= "<td>";
-          $html .= "<input type=\"text\" class=\"form-control\" name=\"total\" value=\"" . ($price * $quantity) . "\" readonly>"; // Calculate total
-          $html .= "</td>";
-          $html .= "<td>";
-          $html .= "<input type=\"date\" class=\"form-control datePicker\" name=\"date\" data-date data-date-format=\"yyyy-mm-dd\">";
-          $html .= "</td>";
-          $html .= "<td>";
-          $html .= "<button type=\"submit\" name=\"add_sale\" class=\"btn btn-primary\">Add sale</button>";
-          $html .= "</td>";
-          $html .= "</tr>";
-      }
-  } else {
-      $html = '<tr><td>Product name not registered in the database.</td></tr>';
+          $html .= "<input type=\"text\" class=\"form-control\" name=\"quantity\" value=\"1\">";
+          $html  .= "</td>";
+          $html  .= "<td>";
+          $html  .= "<input type=\"text\" class=\"form-control\" name=\"total\" value=\"{$result['sale_price']}\">";
+          $html  .= "</td>";
+          $html  .= "<td>";
+          $html  .= "<input type=\"date\" class=\"form-control datePicker\" name=\"date\" data-date data-date-format=\"yyyy-mm-dd\">";
+          $html  .= "</td>";
+          $html  .= "<td>";
+          $html  .= "<button type=\"submit\" name=\"add_sale\" class=\"btn btn-primary\">Add sale</button>";
+          $html  .= "</td>";
+          $html  .= "</tr>";
+
+        }
+    } else {
+        $html ='<tr><td>product name not resgister in database</td></tr>';
+    }
+
+    echo json_encode($html);
   }
-
-  echo json_encode($html);
-}
-
  ?>
