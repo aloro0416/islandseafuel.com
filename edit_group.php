9ac562628e767b55286c6831831284a2733a5456
aloro0416 <abellolordesaloro@gmail.com>
2024-12-09 17:29:20 +0800

    a

--- edit_group.php
+++ edit_group.php
@@ -33,7 +33,7 @@
    if(find_by_groupName($_POST['group-name']) === false ){
     $session->msg('d','<b>Sorry!</b> Entered Group Name already in database!');
     redirect('edit_group?id='.(int)$e_group['id'], false);
-  }elseif(find_by_groupLevel($_POST['group-level']) === false) {
+  }else if(find_by_groupLevel($_POST['group-level']) === false) {
     $session->msg('d','<b>Sorry!</b> Entered Group Level already in database!');
     redirect('edit_group?id='.(int)$e_group['id'], false);
   }
