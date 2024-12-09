523092aab31709bea7d2aa2b4e80a64727188459
aloro0416 <abellolordesaloro@gmail.com>
2024-12-09 13:43:44 +0800

    a

--- ajax.php
+++ ajax.php
@@ -1,68 +1,45 @@
 <?php
   require_once('includes/load.php');
-  if (!$session->isUserLoggedIn(true)) { redirect('.', false);}
-?>
+  if (!$session->isUserLoggedIn(true)) { redirect('.', false); }
 
-<?php
- // Auto suggetion
-    $html = '';
-   if(isset($_POST['product_name']) && strlen($_POST['product_name']))
-   {
-     $products = find_product_by_title($_POST['product_name']);
-     if($products){
-        foreach ($products as $product):
-           $html .= "<li class=\"list-group-item\">";
-           $html .= $product['name'];
-           $html .= "</li>";
-         endforeach;
-      } else {
-
-        $html .= '<li onClick=\"fill(\''.addslashes().'\')\" class=\"list-group-item\">';
-        $html .= 'Not found';
+  // Auto suggestion
+  $html = '';
+  if (isset($_POST['product_name']) && strlen($_POST['product_name'])) {
+    $products = find_product_by_title($_POST['product_name']);
+    if ($products) {
+      foreach ($products as $product):
+        $html .= "<li class=\"list-group-item\" onClick=\"fill('".addslashes($product['name'])."')\">";
+        $html .= $product['name'];
         $html .= "</li>";
-
-      }
-
-      echo json_encode($html);
-   }
- ?>
- <?php
- // find all product
-  if(isset($_POST['p_name']) && strlen($_POST['p_name']))
-  {
-    $product_title = remove_junk($db->escape($_POST['p_name']));
-    if($results = find_all_product_info_by_title($product_title)){
-        foreach ($results as $result) {
-
-          $html .= "<tr>";
-
-          $html .= "<td id=\"s_name\">".$result['name']."</td>";
-          $html .= "<input type=\"hidden\" name=\"s_id\" value=\"{$result['id']}\">";
-          $html  .= "<td>";
-          $html  .= "<input type=\"text\" class=\"form-control\" id=\"price\" name=\"price\" value=\"{$result['sale_price']}\">";
-          $html  .= "</td>";
-          $html .= "<td id=\"s_qty\">";
-          $html .= "<input type=\"text\" class=\"form-control\" id=\"quantity\" name=\"quantity\" value=\"1\">";
-          $html  .= "</td>";
-          $html  .= "<td>";
-          $html  .= "<input type=\"text\" class=\"form-control\" name=\"total\" value=\"{$result['sale_price']}\" readonly>";
-          $html  .= "</td>";
-          $html  .= "<td>";
-          $html  .= "<input type=\"date\" class=\"form-control datePicker\" name=\"date\" data-date data-date-format=\"yyyy-mm-dd\">";
-          $html  .= "</td>";
-          $html  .= "<td>";
-          $html  .= "<button type=\"submit\" name=\"add_sale\" class=\"btn btn-primary\">Add sale</button>";
-          $html  .= "</td>";
-          $html  .= "</tr>";
-
-        }
+      endforeach;
     } else {
-        $html ='<tr><td>product name not resgister in database</td></tr>';
+      $html .= '<li onClick="fill(\'\')" class="list-group-item">Not found</li>';
     }
-
     echo json_encode($html);
   }
- ?>
+
+  // Find all products
+  if (isset($_POST['p_name']) && strlen($_POST['p_name'])) {
+    $product_title = remove_junk($db->escape($_POST['p_name']));
+    if ($results = find_all_product_info_by_title($product_title)) {
+      foreach ($results as $result) {
+        $html .= "<tr>";
+        $html .= "<td id=\"s_name\">" . $result['name'] . "</td>";
+        $html .= "<input type=\"hidden\" name=\"s_id\" value=\"{$result['id']}\">";
+        $html .= "<td><input type=\"text\" class=\"form-control\" id=\"price\" name=\"price\" value=\"{$result['sale_price']}\"></td>";
+        $html .= "<td id=\"s_qty\"><input type=\"text\" class=\"form-control\" id=\"quantity\" name=\"quantity\" value=\"1\"></td>";
+        $html .= "<td><input type=\"text\" class=\"form-control\" name=\"total\" value=\"{$result['sale_price']}\" readonly></td>";
+        $html .= "<td><input type=\"date\" class=\"form-control datePicker\" name=\"date\" data-date data-date-format=\"yyyy-mm-dd\"></td>";
+        $html .= "<td><button type=\"submit\" name=\"add_sale\" class=\"btn btn-primary\">Add sale</button></td>";
+        $html .= "</tr>";
+      }
+    } else {
+      $html = '<tr><td>Product name not registered in the database</td></tr>';
+    }
+    echo json_encode($html);
+  }
+?>
+a
 
 <script>
   document.getElementById('price').addEventListener('input', function () {
