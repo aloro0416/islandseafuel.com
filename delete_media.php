9a12bf3f116f039231b15c0223aa09f6f1f9136c
aloro0416 <abellolordesaloro@gmail.com>
2024-12-09 15:56:33 +0800

    a

--- delete_media.php
+++ delete_media.php
@@ -1,19 +1,4 @@
-<?php
-  require_once('includes/load.php');
-  // Checkin What level user has permission to view this page
-  page_require_level(2);
-?>
-<?php
-  $find_media = find_by_id('media',(int)$_GET['id']);
-  $photo = new Media();
-  if($photo->media_destroy($find_media['id'],$find_media['file_name'])){
-      $session->msg("s","Photo has been deleted.");
-      redirect('media');
-  } else {
-      $session->msg("d","Photo deletion failed Or Missing Prm.");
-      redirect('media');
-  }
-?>
+
 <?php
   require_once('includes/load.php');
   // Check if user has permission to delete products
