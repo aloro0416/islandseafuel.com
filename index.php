9830af4cbe8a0174dd4a2ff9617ace33fcce803e
aloro0416 <abellolordesaloro@gmail.com>
2024-12-09 19:10:32 +0800

    a

--- index.php
+++ index.php
@@ -264,13 +264,13 @@ const formInputs = document.querySelectorAll('#username, #myInput');
   /* Media query for mobile devices */
   @media (max-width: 768px) {
     body {
-      background-size: cover; /* Adjusts the background size for smaller screens */
-      background-position: center; /* Aligns the image to the top center for better display on mobile */
-      background-attachment: fixed; /* Fixes the background scrolling issue on mobile */
+      background-size: contain; /* Adjusts the background size for smaller screens */
+      background-position: top center; /* Aligns the image to the top center for better display on mobile */
+      background-attachment: scroll; /* Fixes the background scrolling issue on mobile */
+      padding: 35px 15px 20px 15px; /* Adjust padding for smaller screens */
+      top: 20px; /* Reduce the top margin for mobile */
+      left: 0;
       width: 100%; /* Ensure full-width on smaller screens */
-    }
-    .login-page {
-        width: 100%;
     }
   }
 
