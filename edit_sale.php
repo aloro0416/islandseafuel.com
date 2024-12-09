c643c5500ccf3ae97b966be5820d152a4c7d7813
aloro0416 <abellolordesaloro@gmail.com>
2024-12-09 11:18:41 +0800

    a

--- edit_sale.php
+++ edit_sale.php
@@ -80,13 +80,13 @@ if(!$sale){
                   <div id="result" class="list-group"></div>
                 </td>
                 <td id="s_qty">
-                  <input type="text" class="form-control" name="quantity" value="<?php echo (int)$sale['qty']; ?>">
+                  <input type="text" class="form-control" id="quantity" name="quantity" value="<?php echo (int)$sale['qty']; ?>">
                 </td>
                 <td id="s_price">
-                  <input type="text" class="form-control" name="price" value="<?php echo remove_junk($product['sale_price']); ?>" >
+                  <input type="text" class="form-control" id="price" name="price" value="<?php echo remove_junk($product['sale_price']); ?>" >
                 </td>
                 <td>
-                  <input type="text" class="form-control" name="total" value="<?php echo remove_junk($sale['price']); ?>">
+                  <input type="text" class="form-control" name="total" value="<?php echo remove_junk($sale['price']); ?>" readonly>
                 </td>
                 <td id="s_date">
                   <input type="date" class="form-control datepicker" name="date" data-date-format="" value="<?php echo remove_junk($sale['date']); ?>">
@@ -104,5 +104,70 @@ if(!$sale){
   </div>
 
 </div>
+
+<script>
+  document.getElementById('sug_input').addEventListener('input', function () {
+        var sug_input = this.value.trim();
+        
+        var dangerousCharsPattern = /[<>\"\']/;
+        
+        if (sug_input === "") {
+            this.setCustomValidity('Product title cannot be empty or just spaces.');
+        } else if (this.value !== sug_input) {
+            this.setCustomValidity('Product title cannot start with a space.');
+        } else if (dangerousCharsPattern.test(sug_input)) {
+            this.setCustomValidity('Product title cannot contain HTML special characters like <, >, ", \'.');
+        } else {
+            this.setCustomValidity('');
+        }
+        
+        var isValid = sug_input !== "" && this.value === sug_input && !dangerousCharsPattern.test(sug_input);
+        this.classList.toggle('is-invalid', !isValid);
+    });
+
+    document.getElementById('quantity').addEventListener('input', function () {
+        var quantity = this.value.trim();
+
+        var dangerousCharsPattern = /[<>\"']/;
+        var numericPattern = /^[0-9]*$/;  // Only allows numbers
+        
+        if (quantity === "") {
+            this.setCustomValidity('Quantity cannot be empty or just spaces.');
+        } else if (this.value !== quantity) {
+            this.setCustomValidity('Quantity cannot start with a space.');
+        } else if (dangerousCharsPattern.test(quantity)) {
+            this.setCustomValidity('Quantity cannot contain HTML special characters like <, >, ", \'.');
+        } else if (!numericPattern.test(quantity)) {
+            this.setCustomValidity('Please enter only numbers.');
+        } else {
+            this.setCustomValidity('');
+        }
+
+        var isValid = quantity !== "" && this.value === quantity && !dangerousCharsPattern.test(quantity) && numericPattern.test(quantity);
+        this.classList.toggle('is-invalid', !isValid);
+    });
+
+    document.getElementById('price').addEventListener('input', function () {
+        var price = this.value.trim();
+
+        var dangerousCharsPattern = /[<>\"']/;
+        var numericPattern = /^[0-9]*$/;  // Only allows numbers
+        
+        if (price === "") {
+            this.setCustomValidity('Price cannot be empty or just spaces.');
+        } else if (this.value !== price) {
+            this.setCustomValidity('Price cannot start with a space.');
+        } else if (dangerousCharsPattern.test(price)) {
+            this.setCustomValidity('Price cannot contain HTML special characters like <, >, ", \'.');
+        } else if (!numericPattern.test(price)) {
+            this.setCustomValidity('Please enter only numbers.');
+        } else {
+            this.setCustomValidity('');
+        }
+
+        var isValid = price !== "" && this.value === price && !dangerousCharsPattern.test(price) && numericPattern.test(quantity);
+        this.classList.toggle('is-invalid', !isValid);
+    });
+</script>
 
 <?php include_once('layouts/footer.php'); ?>
