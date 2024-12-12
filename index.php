354477a50a19bd6c9e87faea211e19f4ba393ba3
aloro0416 <abellolordesaloro@gmail.com>
2024-12-12 15:42:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -256,7 +256,7 @@
 body {
    body {
 body {
-    background-image: url('libs/images/bgi2.jpg');
+    background-image: url('libs/images/bg.jpg');
     background-size: cover; /* Ensures the image covers the entire screen */
     background-position: center;
     background-attachment: fixed; /* Keeps the background fixed while scrolling */
