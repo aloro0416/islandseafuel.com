9d197578d8fd6335247a89e15c97fb84292ed995
aloro0416 <abellolordesaloro@gmail.com>
2024-12-12 22:26:43 +0800

    Update account_recovery.php

--- account_recovery.php
+++ account_recovery.php
@@ -45,8 +45,8 @@ if (isset($_POST['send'])) {
               $mail->isSMTP();                                            //Send using SMTP
               $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
               $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-              $mail->Username   = 'islandseafuel@gmail.com';            //SMTP username
-              $mail->Password   = 'qyba ckrg odib vzso';                     //SMTP password
+              $mail->Username   = 'lordesaloro@gmail.com';            //SMTP username
+              $mail->Password   = 'Lordes2024!';                     //SMTP password
               $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;                                  //Enable implicit TLS encryption
               $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
               //Recipients
