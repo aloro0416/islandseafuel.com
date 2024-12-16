3b01ed4a2d208b26695d073d3a7b6a8825aa2fa2
aloro0416 <abellolordesaloro@gmail.com>
2024-12-16 21:02:27 +0800

    z

--- pos.php
+++ pos.php
@@ -1,5 +1,5 @@
 <?php
-  $page_title = 'POS';
+//   $page_title = 'POS';
   require_once('includes/load.php');
   // Checkin What level user has permission to view this page
   page_require_level(2);
