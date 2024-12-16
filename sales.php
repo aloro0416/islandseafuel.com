01247974c4022ac2230b472a12706df364cda974
aloro0416 <abellolordesaloro@gmail.com>
2024-12-16 17:53:21 +0800

    udapse

--- sales.php
+++ sales.php
@@ -1,5 +1,5 @@
 <?php
-  $page_title = 'All sale';
+  // $page_title = 'All sale';
   require_once('includes/load.php');
   // Checkin What level user has permission to view this page
    page_require_level(3);
