ca3736a18af3acb8614d89ed0e0534e8e8469079
aloro0416 <abellolordesaloro@gmail.com>
2024-12-16 21:52:10 +0800

    asdsad

--- layouts/admin_menu.php
+++ layouts/admin_menu.php
@@ -1,216 +1,95 @@
-<style>
-  /* Sidebar Styling */
-.sidebar {
-  width: 250px;
-  background-color: #2c3e50;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top: 20px;
-  color: #ecf0f1;
-  transition: width 0.3s;
-  height: 100%;
-}
+<!-- Bootstrap CSS -->
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 
-.sidebar a {
-  display: block;
-  padding: 15px;
-  text-decoration: none;
-  color: #ecf0f1;
-  font-size: 16px;
-  transition: background-color 0.3s;
-}
+<!-- Bootstrap JS and jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-.sidebar a:hover {
-  background-color: #34495e;
-}
 
-.sidebar .submenu-toggle {
-  cursor: pointer;
-}
-
-/* Hide submenu by default */
-.sidebar .submenu {
-  display: none;
-  padding-left: 20px;
-}
-
-.sidebar .submenu li {
-  margin: 5px 0;
-}
-
-.sidebar .submenu-toggle.active:after {
-  content: " ▲";
-}
-
-.sidebar .submenu-toggle:after {
-  content: " ▼";
-}
-
-.sidebar .submenu-toggle.active + .submenu {
-  display: block;
-}
-
-/* Mobile Sidebar: Collapsed View */
-@media (max-width: 768px) {
-  .sidebar {
-    width: 60px;
-    transition: width 0.3s;
-  }
-
-  .sidebar a span {
-    display: none;
-  }
-
-  .sidebar .submenu {
-    position: absolute;
-    top: 0;
-    left: 60px;
-    background-color: #34495e;
-    width: 200px;
-    display: none;
-  }
-
-  .sidebar .submenu li {
-    padding-left: 20px;
-  }
-
-  .sidebar .submenu-toggle.active + .submenu {
-    display: block;
-  }
-
-  /* Hamburger Menu (mobile) */
-  .menu-toggle {
-    display: block;
-    position: absolute;
-    top: 20px;
-    left: 20px;
-    cursor: pointer;
-  }
-
-  .menu-toggle span {
-    background-color: #ecf0f1;
-    display: block;
-    height: 4px;
-    width: 25px;
-    margin: 5px 0;
-  }
-
-  .sidebar .logo {
-    display: none;
-  }
-
-  .sidebar.active {
-    width: 250px;
-  }
-
-  .sidebar.active .logo {
-    display: block;
-    text-align: center;
-    margin-top: 20px;
-  }
-
-  /* Display menu with icons only */
-  .sidebar.active a span {
-    display: inline-block;
-  }
-
-  .sidebar .submenu {
-    position: relative;
-  }
-}
-
-</style>
-<nav class="sidebar">
-  <button class="menu-toggle">
-    <span></span>
-    <span></span>
-    <span></span>
+<nav class="sidebar navbar navbar-expand-md navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
   </button>
-  <ul>
-    <li>
-      <a href="admin">
-        <i class="glyphicon glyphicon-home"></i>
-        <span>Dashboard</span>
-      </a>
-    </li>
-    <li>
-      <a href="pos">
-        <i class="glyphicon glyphicon-indent-left"></i>
-        <span>POS</span>
-      </a>
-    </li>
-    <li class="submenu">
-      <a href="#" class="submenu-toggle">
-        <i class="glyphicon glyphicon-th-large"></i>
-        <span>Products</span>
-      </a>
-      <ul class="nav submenu">
-        <li><a href="product">Manage Products</a></li>
-        <li><a href="add_product">Add Products</a></li>
-      </ul>
-    </li>
-    <li class="submenu">
-      <a href="#" class="submenu-toggle">
-        <i class="glyphicon glyphicon-credit-card"></i>
-        <span>Sales</span>
-      </a>
-      <ul class="nav submenu">
-        <li><a href="sales">Manage Sales</a></li>
-        <li><a href="add_sale">Add Sale</a></li>
-      </ul>
-    </li>
-    <li class="submenu">
-      <a href="#" class="submenu-toggle">
-        <i class="glyphicon glyphicon-duplicate"></i>
-        <span>Sales Report</span>
-      </a>
-      <ul class="nav submenu">
-        <li><a href="sales_report">Sales by Dates</a></li>
-        <li><a href="monthly_sales">Monthly Sales</a></li>
-        <li><a href="daily_sales">Daily Sales</a></li>
-        <li><a href="product_sales">Product Sales</a></li>
-      </ul>
-    </li>
-    <li>
-      <a href="categorie">
-        <i class="glyphicon glyphicon-indent-left"></i>
-        <span>Categories</span>
-      </a>
-    </li>
-    <li>
-      <a href="media">
-        <i class="glyphicon glyphicon-picture"></i>
-        <span>Media Files</span>
-      </a>
-    </li>
-    <li class="submenu">
-      <a href="#" class="submenu-toggle">
-        <i class="glyphicon glyphicon-user"></i>
-        <span>User Management</span>
-      </a>
-      <ul class="nav submenu">
-        <li><a href="group">Manage Groups</a></li>
-        <li><a href="users">Manage Users</a></li>
-      </ul>
-    </li>
-    <li class="logo">
-      <img src="libs/images/logo.png" alt="ISLAND SEA LOGO" style="height: 100px;">
-    </li>
-  </ul>
+  
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link" href="admin">
+          <i class="glyphicon glyphicon-home"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pos">
+          <i class="glyphicon glyphicon-indent-left"></i>
+          <span>POS</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link submenu-toggle" href="#" data-toggle="collapse" data-target="#productsMenu">
+          <i class="glyphicon glyphicon-th-large"></i>
+          <span>Products</span>
+        </a>
+        <div id="productsMenu" class="collapse">
+          <ul class="nav flex-column ml-3">
+            <li class="nav-item"><a class="nav-link" href="product">Manage Products</a></li>
+            <li class="nav-item"><a class="nav-link" href="add_product">Add Products</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link submenu-toggle" href="#" data-toggle="collapse" data-target="#salesMenu">
+          <i class="glyphicon glyphicon-credit-card"></i>
+          <span>Sales</span>
+        </a>
+        <div id="salesMenu" class="collapse">
+          <ul class="nav flex-column ml-3">
+            <li class="nav-item"><a class="nav-link" href="sales">Manage Sales</a></li>
+            <li class="nav-item"><a class="nav-link" href="add_sale">Add Sale</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link submenu-toggle" href="#" data-toggle="collapse" data-target="#salesReportMenu">
+          <i class="glyphicon glyphicon-duplicate"></i>
+          <span>Sales Report</span>
+        </a>
+        <div id="salesReportMenu" class="collapse">
+          <ul class="nav flex-column ml-3">
+            <li class="nav-item"><a class="nav-link" href="sales_report">Sales by Dates</a></li>
+            <li class="nav-item"><a class="nav-link" href="monthly_sales">Monthly Sales</a></li>
+            <li class="nav-item"><a class="nav-link" href="daily_sales">Daily Sales</a></li>
+            <li class="nav-item"><a class="nav-link" href="product_sales">Product Sales</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="categorie">
+          <i class="glyphicon glyphicon-indent-left"></i>
+          <span>Categories</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="media">
+          <i class="glyphicon glyphicon-picture"></i>
+          <span>Media Files</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link submenu-toggle" href="#" data-toggle="collapse" data-target="#userMenu">
+          <i class="glyphicon glyphicon-user"></i>
+          <span>User Management</span>
+        </a>
+        <div id="userMenu" class="collapse">
+          <ul class="nav flex-column ml-3">
+            <li class="nav-item"><a class="nav-link" href="group">Manage Groups</a></li>
+            <li class="nav-item"><a class="nav-link" href="users">Manage Users</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <img src="libs/images/logo.png" alt="ISLAND SEA LOGO" style="height: 100px; margin-top: 50px;">
+      </li>
+    </ul>
+  </div>
 </nav>
-
-<script>
-  // Toggle submenu visibility
-document.querySelectorAll('.submenu-toggle').forEach(item => {
-  item.addEventListener('click', function() {
-    this.classList.toggle('active');
-  });
-});
-
-// Toggle sidebar on small screens (Hamburger menu)
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-  document.querySelector('.sidebar').classList.toggle('active');
-});
-
-</script>
