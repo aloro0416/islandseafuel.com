5960ab1a7e8a3549fb102f4d8223b9d0d0e40d66
aloro0416 <abellolordesaloro@gmail.com>
2024-12-17 08:57:14 +0800

    sa

--- admin.php
+++ admin.php
@@ -241,6 +241,64 @@
         </script>
       </div>
     </div>
+    <div class="row">
+      
+      <div class="col">
+        <div class="panel panel-box clearfix" style="padding: 10px;">
+          <!-- Second Chart: Pie Chart -->
+          <div id="pieChart"></div>
+        </div>
+
+        <script>
+          var options = {
+            series: [
+              <?=$january?>, <?=$febuary?>, <?=$march?>, <?=$april?>, <?=$may?>, <?=$june?>, <?=$july?>, <?=$aug?>, <?=$sept?>, <?=$oct?>, <?=$nov?>, <?=$dec?>,
+              <?= $product_sales_json ?>,  // Product sales data for Premium
+              <?= $product_sales_json_2 ?>, // Product sales data for Diesel
+              <?= $product_sales_json_3 ?>  // Product sales data for Super93
+            ],
+            chart: {
+              height: 350,
+              type: 'pie', // Change the chart type to pie
+            },
+            labels: [
+              'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',
+              'Premium', 'Diesel', 'Super93'
+            ],
+            dataLabels: {
+              enabled: true,
+              formatter: function(val) {
+                return "₱ " + val.toFixed(2);  // Format the values with currency
+              },
+              style: {
+                fontSize: '12px',
+                colors: ["#304758"]
+              }
+            },
+            tooltip: {
+              enabled: true,
+              y: {
+                formatter: function(val) {
+                  return "₱ " + val.toFixed(2);  // Tooltip formatting with currency
+                }
+              }
+            },
+            title: {
+              text: 'Sales Distribution',
+              floating: true,
+              offsetY: 330,
+              align: 'center',
+              style: {
+                color: '#444'
+              }
+            }
+          };
+
+          var chart2 = new ApexCharts(document.querySelector("#pieChart"), options);
+          chart2.render();
+        </script>
+      </div>
+    </div>
       </div>
 
 
