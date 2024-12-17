3c88b4d20196eface89b9ded98c776761577b0de
aloro0416 <abellolordesaloro@gmail.com>
2024-12-17 22:43:43 +0800

    asd

--- product_sales.php
+++ product_sales.php
@@ -106,7 +106,7 @@
                                 <img src="libs/images/logo.png" alt="Company Logo" style="width: 150px;">
                                 <h2>ISLAND SEA FUEL</h2>
                                 <h4>KANGWAYAN MADRIDEJOS CEBU</h4>
-                                  <h4>Product Saless</h4>
+                                  <h4>Product Sales</h4>
                             </div>
                         `);
                         $(win.document.body).find('table')
