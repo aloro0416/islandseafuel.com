082a01a6583ba90e1689df762cf9aa11182b5d7e
aloro0416 <abellolordesaloro@gmail.com>
2024-12-17 08:15:04 +0800

    asd

--- admin.php
+++ admin.php
@@ -393,93 +393,47 @@
             <div id="chart"></div>
             <script>
               var options = {
-                series: [{
-                name: 'Amount',
-                data: [<?=$total_bought?>, <?=$total_sales?>]
-              }],
+                series: [<?=$total_bought?>, <?=$total_sales?>],
                 chart: {
-                height: 350,
-                type: 'pie',
-              },
-              plotOptions: {
-                bar: {
-                  columnWidth: '45%',
-                  borderRadius: 10,
-                  dataLabels: {
-                    position: 'top', // top, center, bottom
+                  height: 350,
+                  type: 'pie', // Corrected to 'pie' from 'bar'
+                },
+                labels: ['Available Gas', 'Sales'], // Labels for the pie chart
+                dataLabels: {
+                  enabled: true,
+                  formatter: function (val) {
+                    return "₱ " + val; // Currency formatting for pie chart values
                   },
-                }
-              },
-              dataLabels: {
-                enabled: true,
-                formatter: function (val) {
-                  return "₱ " + val;
-                },
-                offsetY: -20,
-                style: {
-                  fontSize: '12px',
-                  colors: ["#304758"]
-                }
-              },
-              
-              xaxis: {
-                categories: ["Available Gas", "Sales"],
-                position: 'top',
-                axisBorder: {
-                  show: false
-                },
-                axisTicks: {
-                  show: false
-                },
-                crosshairs: {
-                  fill: {
-                    type: 'gradient',
-                    gradient: {
-                      colorFrom: '#D8E3F0',
-                      colorTo: '#BED1E6',
-                      stops: [0, 100],
-                      opacityFrom: 0.4,
-                      opacityTo: 0.5,
-                    }
+                  style: {
+                    fontSize: '12px',
+                    colors: ["#304758"]
                   }
                 },
                 tooltip: {
                   enabled: true,
-                }
-              },
-              
-              yaxis: {
-                axisBorder: {
-                  show: false
+                  y: {
+                    formatter: function (val) {
+                      return "₱ " + val; // Tooltip currency format
+                    }
+                  }
                 },
-                axisTicks: {
-                  show: false,
-                },
-                labels: {
-                  show: false,
-                  formatter: function (val) {
-                    return "₱ " + val;
+                title: {
+                  text: 'OVERALL SALES',
+                  floating: true,
+                  offsetY: 330,
+                  align: 'center',
+                  style: {
+                    color: '#444'
                   }
                 }
-              
-              },
-              title: {
-                text: 'OVER ALL SALES',
-                floating: true,
-                offsetY: 330,
-                align: 'center',
-                style: {
-                  color: '#444'
-                }
-              }
               };
 
               var chart = new ApexCharts(document.querySelector("#chart"), options);
               chart.render();
             </script>
-            </div>
           </div>
         </div>
+
   <div class="row">
     <div class="col-md-6">
       <div class="panel panel-default">
